69a2011461faa2cb8af2e2b53d5ce652318acd2d
Siu0901 <trip0901@naver.com>
2026-03-17 22:01:43 +0900

    feat: 녹음 곡 바탕 추천 시스템 추가

--- app/api/v1/endpoints/record/record_create.py
+++ app/api/v1/endpoints/record/record_create.py
@@ -9,26 +9,26 @@ from app.services.AI_models.SAT_model.SAT_predict import FSpredictor
 
 create_record_router = APIRouter()
 
-@create_record_router.post("/record/create")
-async def create_record(
-        request: RecordCreateRequest,
-        userdata: Annotated[dict, Depends(jwt_manager.check_token)],
-        session: SessionDep
-):
-    song_chord = Cpredictor.predict_result(request.file_url)
-    song_style_speed = FSpredictor.analyze_guitar_performance(request.file_url)
-
-    record = UserRecord(
-        name=request.name,
-        style=song_style_speed.style,
-        chord=song_chord,
-        speed=song_style_speed.tempo,
-        file_url=request.file_url,
-        spec_img_url=request.spec_img_url,
-        user_id=userdata["sub"],
-    )
-
-    session.add(record)
-    session.commit()
-    session.refresh(record)
-    return record
+# @create_record_router.post("/record/create")
+# async def create_record(
+#         request: RecordCreateRequest,
+#         userdata: Annotated[dict, Depends(jwt_manager.check_token)],
+#         session: SessionDep
+# ):
+#     song_chord = Cpredictor.predict_result(request.file_url)
+#     song_style_speed = FSpredictor.analyze_guitar_performance(request.file_url)
+#
+#     record = UserRecord(
+#         name=request.name,
+#         style=song_style_speed.style,
+#         chord=song_chord,
+#         speed=song_style_speed.tempo,
+#         file_url=request.file_url,
+#         spec_img_url=request.spec_img_url,
+#         user_id=userdata["sub"],
+#     )
+#
+#     session.add(record)
+#     session.commit()
+#     session.refresh(record)
+#     return record
--- app/api/v1/endpoints/record/record_info.py
+++ app/api/v1/endpoints/record/record_info.py
@@ -1,22 +1,28 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from typing import Annotated
 from app.api.depends import SessionDep
 from app.core.security.jwt_token import jwt_manager
 from app.models.postgresDB.user import User
 from app.models.postgresDB.user_record import UserRecord
+from app.services.reco_system.record_recommend import record_recommender
 
 record_info_router = APIRouter()
 
 @record_info_router.get("/record/list")
-async def record_info(session: SessionDep, userdata: Annotated[dict, Depends(jwt_manager.check_token)]):
+async def record_info(
+        session: SessionDep,
+        userdata: Annotated[dict, Depends(jwt_manager.check_token)]
+):
     user = session.get(User, userdata["sub"])
     return user.records
+
 
 @record_info_router.get("/record/info/{record_id}")
 async def record_info(
         session: SessionDep,
         record_id: int,
-        userdata: Annotated[dict, Depends(jwt_manager.check_token)]
+        userdata: Annotated[dict, Depends(jwt_manager.check_token)],
+        limit: int = Query(default=12),
 ):
     record = session.get(UserRecord, record_id)
     if not record:
@@ -24,4 +30,12 @@ async def record_info(
     if record.user_id != int(userdata["sub"]):
         raise HTTPException(status_code=403, detail="User not found")
 
-    return record
+    analysis = {
+        "style": record.style,
+        "tempo": record.speed,
+        "chords": record.chord,
+    }
+
+    results = record_recommender.recommend(session, user_level=13, analysis=analysis, limit=limit)
+
+    return {"record_info": record, "recommend": results}
