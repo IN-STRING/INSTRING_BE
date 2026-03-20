87f3dc3417c3d732dfd2b6a825894f2edaa2e4b0
Siu0901 <trip0901@naver.com>
2026-03-21 00:28:03 +0900

    fix: 녹음 리스트 출력 형식 수정

--- app/api/v1/endpoints/record/record_info.py
+++ app/api/v1/endpoints/record/record_info.py
@@ -1,20 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from typing import Annotated
+from sqlmodel import select
 from app.api.depends import SessionDep
 from app.core.security.jwt_token import jwt_manager
 from app.models.postgresDB.user import User
+from app.schemas.record_dto import SearchRecords
 from app.models.postgresDB.user_record import UserRecord
 from app.services.reco_system.record_recommend import record_recommender
 
 record_info_router = APIRouter()
 
-@record_info_router.get("/record/list")
+@record_info_router.get("/record/list", response_model=SearchRecords)
 async def record_info(
         session: SessionDep,
         userdata: Annotated[dict, Depends(jwt_manager.check_token)]
 ):
-    user = session.get(User, userdata["sub"])
-    return user.records
+    stmt = select(UserRecord).where(UserRecord.user_id == userdata["sub"])
+    records = session.exec(stmt).all()
+    return {"records": records}
 
 
 @record_info_router.get("/record/info/{record_id}")
